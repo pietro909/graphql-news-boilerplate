6391a6a6dafc8f256b777f89fed9f8d8f7c51ef6
Pietro Grandi <pietro.grandi.3d@gmail.com>
2019-10-19 10:51:52 +0200

    Circular references

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
-import {makeExecutableSchema} from 'graphql-tools';
 import {find} from 'lodash';
+import {
+  GraphQLInt,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+  GraphQLList,
+} from 'graphql';
 
 const typeDefs = `
   type Link {
@@ -108,7 +115,82 @@ function getComments(commentID) {
   return comments;
 }
 
-const schema = makeExecutableSchema({typeDefs, resolvers});
+const NonNull = t => new GraphQLNonNull(t);
+const IDType = NonNull(GraphQLInt);
+
+const userType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: {type: IDType},
+    username: {type: NonNull(GraphQLString)},
+    about: {type: NonNull(GraphQLString)},
+  },
+});
+
+const commentsType = new GraphQLObjectType({
+  name: 'Comments',
+  fields: () => ({
+    id: {type: IDType},
+    parent: {type: commentsType},
+    comments: {
+      type: NonNull(new GraphQLList(commentsType)),
+      args: {id: {type: GraphQLInt}},
+      resolve: (_, {id}) => getComments(id),
+    },
+    author: {
+      type: NonNull(userType),
+      args: {author: {type: GraphQLInt}},
+      resolve: ({author}) => find(DB.users, {id: author}),
+    },
+    content: {type: NonNull(GraphQLString)},
+  }),
+});
+
+const linkType = new GraphQLObjectType({
+  name: 'Link',
+  fields: {
+    id: {type: IDType},
+    url: {type: NonNull(GraphQLString)},
+    description: {type: NonNull(GraphQLString)},
+    author: {
+      type: NonNull(userType),
+      args: {author: {type: GraphQLInt}},
+      resolve: ({author}) => find(DB.users, {id: author}),
+    },
+    comments: {
+      type: NonNull(new GraphQLList(commentsType)),
+      resolve: ({comments}) => comments.map(id => find(DB.comments, {id})),
+    },
+  },
+});
+
+const queryType = new GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    allLinks: {
+      type: NonNull(new GraphQLList(linkType)),
+      resolve: () => DB.links,
+    },
+    link: {
+      type: linkType,
+      args: {
+        id: {type: IDType},
+      },
+      resolve: (_, {id}) => find(DB.links, {id}),
+    },
+    user: {
+      type: userType,
+      args: {id: {type: IDType}},
+      resolve: (_, {id}) => find(DB.users, {id}),
+    },
+    allUsers: {
+      type: new GraphQLList(userType),
+      resolve: () => DB.users,
+    },
+  },
+});
+
+const schema = new GraphQLSchema({query: queryType});
 
 const app = express();
 
