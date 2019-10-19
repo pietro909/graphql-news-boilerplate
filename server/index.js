bdda096968320fee09d64948fd30795921e671b9
Pietro Grandi <pietro.grandi.3d@gmail.com>
2019-10-19 10:26:40 +0200

    Recursive fields

--- server/index.js
+++ server/index.js
@@ -9,12 +9,21 @@ const typeDefs = `
     url: String!
     description: String!
     author: User
+    comments: [Comment!]!
   }
 
   type User {
     id: Int! @unique
     username: String!
     age: Int!
+  }
+
+  type Comment {
+    id: Int! @unique
+    parent: Comment
+    comments: [Comment!]!
+    author: User!
+    content: String!
   }
 
   type Query {
@@ -25,22 +34,38 @@ const typeDefs = `
   }
 `;
 
-const links = [
-  {id: 0, author: 1, url: 'https://example.com', description: 'a website'},
-  {
-    id: 1,
-    author: 2,
-    url: 'https://example2.com',
-    description: 'another website',
-  },
-];
+const DB = {
+  links: [
+    {
+      id: 0,
+      author: 1,
+      url: 'https://example.com',
+      description: 'a website',
+      comments: [0, 4],
+    },
+    {
+      id: 1,
+      author: 2,
+      url: 'https://example2.com',
+      description: 'another website',
+      comments: [],
+    },
+  ],
 
-const users = [
-  {id: 0, username: 'pietro', age: 36},
-  {id: 1, username: 'andrea', age: 34},
-  {id: 2, username: 'ettore', age: 31},
-  {id: 3, username: 'lisa', age: 25},
-];
+  users: [
+    {id: 0, username: 'pietro', age: 36},
+    {id: 1, username: 'andrea', age: 34},
+    {id: 2, username: 'ettore', age: 31},
+    {id: 3, username: 'lisa', age: 25},
+  ],
+  comments: [
+    {id: 0, parent: null, author: 0, content: 'chocolate is good'},
+    {id: 1, parent: 0, author: 1, content: "but that's fatty"},
+    {id: 2, parent: 1, author: 0, content: 'yay but I cannot resist'},
+    {id: 3, parent: 0, author: 2, content: 'have you tried the raw one?'},
+    {id: 4, parent: null, author: 2, content: 'what about kunafa?'},
+  ],
+};
 
 /**
  * Resolve function arguments
@@ -59,15 +84,29 @@ const users = [
  */
 const resolvers = {
   Query: {
-    allLinks: () => links,
-    link: (_, {id}) => find(links, {id}),
-    allUsers: () => users,
-    user: (_, {id}) => find(users, {id}),
+    allLinks: () => DB.links,
+    link: (_, {id}) => find(DB.links, {id}),
+    allUsers: () => DB.users,
+    user: (_, {id}) => find(DB.users, {id}),
   },
   Link: {
-    author: ({author}) => find(users, {id: author}),
+    author: ({author}) => find(DB.users, {id: author}),
+    // TODO: this may result in nullable items within the array? It is caught by GraphQL at runtime, at least
+    comments: ({comments}) => comments.map(id => find(DB.comments, {id})),
+  },
+  Comment: {
+    author: ({author}) => find(DB.users, {id: author}),
+    comments: ({id}) => getComments(id),
   },
 };
+
+/**
+ * This is a _thunk_ lazily evaluated
+ */
+function getComments(commentID) {
+  const comments = DB.comments.filter(comment => comment.parent === commentID);
+  return comments;
+}
 
 const schema = makeExecutableSchema({typeDefs, resolvers});
 
